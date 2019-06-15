66ee1015633cdd34e8c2aa072c82998b30344bd0
Anonymous <adiprabhawale@gmail.com>
2019-06-15 20:44:00 +0530

    third commit

---
+++ app/src/main/java/anonymous/teammavericks/secondpage.java
@@ -0,0 +1,13 @@
+package anonymous.teammavericks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class secondpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondpage);
+    }
+}
